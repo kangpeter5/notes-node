267b61e9816e0c65606e69cd23390fc8d2d62266
Peter Kang <peter.kang@cmlmediasoft.local>
2017-05-17 15:35:27 -0700

    using yargs

--- notes.js
+++ notes.js
@@ -1,1 +1,22 @@
 console.log('Starting notes.js');
+
+var addNote = (title, body) => {
+	console.log("Adding Note", title, body);
+};
+var getAll = () => {
+	console.log("Retrieving all Notes");
+};
+var getNote = (title) => {
+	console.log("Getting note: ", title);
+};
+var removeNote = (title) => {
+	console.log('Removing note: ', title);
+};
+
+module.exports = {
+	// addNote: addNote same as below for ES6
+	addNote,
+	getAll,
+	getNote,
+	removeNote
+};
